691c3ace77a28629e1efd74028bde2c57b8b7086
shashank singh <shashakchandel@gmail.com>
2019-11-16 17:54:04 +0530

    secondcommit

--- xorprofit.cpp
+++ xorprofit.cpp
@@ -1,2 +1,35 @@
 
+#include<iostream>
+using namespace std;
 
+
+
+int maxor(int a,int b){
+
+        int ans=0;
+    for(int i=a;i<b;i++){
+
+        ans=max(ans,i^(i+1));
+
+    }
+
+
+return ans;
+
+}
+
+
+
+
+
+
+
+int main(){
+
+int a,b;
+cin>>a>>b;
+cout<<maxor(a,b);
+
+
+return 0;
+}
