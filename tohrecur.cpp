7ddb69194ef88296f7415308deb5a00824c91c30
shashank singh <shashakchandel@gmail.com>
2019-11-23 17:12:45 +0530

    nextcommit

--- tohrecur.cpp
+++ tohrecur.cpp
@@ -1,1 +1,45 @@
 
+
+#include<iostream>
+using namespace std;
+
+
+void tohrecur(int n,char s,char d,char h){
+
+/// if(n=0) return;          this also can be used
+
+    if(n==1)
+    {
+
+        cout<<"Move"<<n<<"-->"<<s<<"from"<<d<<endl;
+        return;
+
+    }
+
+    tohrecur(n-1,s,h,d);
+      cout<<"Move"<<n<<"-->"<<s<<"from"<<d<<endl;
+    tohrecur(n-1,h,d,s);
+
+
+
+
+
+
+}
+
+
+
+
+
+int main(){
+
+int n;
+
+cin>>n;
+
+tohrecur(n,'A','C','B');
+
+
+
+return 0;
+}
