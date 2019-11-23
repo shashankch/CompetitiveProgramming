b277c15164f2ec473a5673747e2728428cd77c68
shashank singh <shashakchandel@gmail.com>
2019-11-24 01:54:32 +0530

    nextcommit

---
+++ recursubsequences.cpp
@@ -0,0 +1,40 @@
+
+#include<iostream>
+using namespace std;
+
+
+void subseq(char *str,char * out,int i,int j){
+
+
+    if(str[i]=='\0'){
+        out[j]='\0';
+        cout<<out<<endl;
+        return;
+    }
+
+    out[j]=str[i];
+   subseq(str,out,i+1,j+1);
+
+
+   subseq(str,out,i+1,j);
+
+
+
+
+}
+
+
+
+
+
+
+int main(){
+
+char str[100];
+char out[100];
+cin>>str;
+subseq(str,out,0,0);
+
+
+return 0;
+}
---
+++ testmarcha1.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int t,n,k;
+    cin>>t;
+    while(t--){
+
+       bool flag=false;
+        cin>>n>>k;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+
+        for(int i=1;i<=(1<<n)-1;i++){
+         int csum=0;
+            int mask=1;
+    for(int j=0;j<31;j++){
+
+        if(i&(mask<<j)){
+            csum+=arr[j];
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+ if(csum==k) {flag=true;
+
+     break;
+ }
+
+
+    }
+
+        if(flag){
+            cout<<"Yes";
+        }
+        else{
+            cout<<"No";
+        }
+        cout<<endl;
+    }
+
+	// your code goes here
+	return 0;
+}
