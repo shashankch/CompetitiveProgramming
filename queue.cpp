8ecf84752c184f0fd6ca421f75b5320fb9e0a4e0
shashank singh <shashakchandel@gmail.com>
2019-12-07 23:38:20 +0530

    nextcommit

---
+++ queue.cpp
@@ -0,0 +1,88 @@
+
+#include<iostream>
+using namespace std;
+
+template<typename T>
+class queue{
+
+    T *arr;
+    int front;
+    int rear;
+    int cs;
+    int ms;
+
+public :
+
+    queue(int ds=4){
+
+
+        front=0;
+        rear=ms-1;
+        cs=0;
+        ms=ds;
+        arr=new T[ms];
+
+    }
+
+    bool isFull(){
+
+        return cs==ms;
+
+    }
+
+    bool isEmpty(){
+
+        return cs==0;
+    }
+
+    void push(T data){
+
+        if(!isFull()){
+            rear=(rear+1)%ms;
+            arr[rear]=data;
+            cs++;
+        }
+    }
+
+    void pop(){
+
+        if(!isEmpty()){
+            front=(front+1)%ms;
+            cs--;
+        }
+    }
+
+    T getFront(){
+
+        return arr[front];
+    }
+};
+
+int main(){
+
+queue <int>q2;
+queue <int> q(10);
+for(int i=0;i<=6;i++){
+
+    q.push(i);
+
+}
+q.pop();
+q.push(8);
+
+while(!q.isEmpty()){
+
+    cout<<q.getFront()<<" ";
+    q.pop();
+}
+
+
+
+
+return 0;
+}
+
+/// for implementation of queue using linked list insert element at tail keep track of tail.
+/// and head for the front ...
+/// implement queue using circular queue by traversing (front or rear+1)%N for front and rear ...here using fixed size array..
+///implemented using dynamic array..
---
+++ stack.cpp
@@ -0,0 +1,73 @@
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+template<typename T,typename X>
+class stack{
+
+private:
+    vector<T> v;
+    vector<X> v2;
+
+public :
+
+    void push(T data){
+
+        v.push_back(data);
+
+    }
+
+    void pop(){
+
+        if(!empty()){
+        v.pop_back();
+        }
+    }
+
+    T  top(){
+
+        return v[v.size()-1];
+
+
+
+    }
+    bool empty(){
+
+    return v.size()==0;
+
+    }
+
+
+
+
+
+
+
+};
+
+
+int main(){
+
+stack <char,int>s;
+
+for(int i=65;i<=70;i++){
+    s.push(i);
+}
+//s.push(1);
+//s.push(2);
+//s.push(3);
+//s.push(4);
+//s.push(5);
+//s.pop();
+//cout<<s.top();
+while(!s.empty()){
+    cout<<s.top()<<" ";
+    s.pop();
+}
+return 0;
+}
+/// implementation of stack using linked list is expensive when elements are inserted at tail..
+/// for implementation of stack using linked list insert element at head and for deletion just move the head ....
+/// for implementation using array just restrict the array to return element at any index...
