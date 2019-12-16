c906804ce5e85a3595094b6f450e8c1efe9d4075
shashank singh <shashakchandel@gmail.com>
2019-12-16 22:47:38 +0530

    nextcommit

--- hungryfish.cpp
+++ hungryfish.cpp
@@ -2,32 +2,74 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
 
 
 
 
-int hungrymoves(vector<int>faqua,int ifish){
+int hungrymovesrecur(int ifish,vector<int>&faqua,int index,int moves){
 
-    int moves=0;
-    for(int i=0;i<faqua.size();i++){
+        ///base case
 
-        if(ifish>faqua[i]){
-            faqua[i]=ifish+faqua[i];
-            ifish=ifish+faqua[i];
+        if(index==faqua.size()){
 
+            return moves;
         }
+
+        while(index<faqua.size() && ifish>faqua[index]){
+
+            ifish+=faqua[index];
+
+            index++;
+            }
+   // if(index==faqua.size()){
+
+           // return moves;
+       // }
+
+        int newifish=ifish+ifish-1;
+        int removal=faqua.size()-1-index;
+        return min(hungrymovesrecur(newifish,faqua,index,moves+1),moves+removal+1);
+        }
+
+
+
+
+
+
+
+
+int hungrymoves(vector<int>&faqua,int ifish){
+
+    sort(faqua.begin(),faqua.end());
+    int sum=ifish;
+    int removalneed=faqua.size();
+    int moves=0;
+    int best=removalneed;
+    int i=0;
+
+    while(i<faqua.size()){
+
+        if(sum>faqua[i]){
+            sum+=faqua[i];
+
+            removalneed-=1;
+            i+=1;
+        }
+        else{
+
+            sum+=(sum-1);
+            moves+=1;
+        }
+        best=min(best,moves+removalneed);
+
     }
-    if(faqua.size()==2){
-        moves+=1;
-    }
-    else{
 
 
-    }
 
-
+    return best;
 
 
 }
@@ -45,23 +87,23 @@ while(getline(check,temp,'#')){
     tokens.push_back(temp);
 }
 int ifish=stoi(tokens[0]);
-cout<<ifish<<endl;
+///cout<<ifish<<endl;
 vector<string>fishnum;
 string fish=tokens[1];
 stringstream check2(fish);
 while(getline(check2,temp,',')){
     fishnum.push_back(temp);
 }
-vector<int>aquarium;
+//vector<int>aquarium;
 for(int i=0;i<fishnum.size();i++){
 
      faqua.push_back(stoi(fishnum[i]));
-      aquarium.push_back(stoi(fishnum[i]));
-    cout<<faqua[i]<<endl;
+    //  aquarium.push_back(stoi(fishnum[i]));
+    ///cout<<faqua[i]<<endl;
 }
-aquarium.push_back()
+//aquarium.push_back()
 
-cout<<hungryfish(faqua,ifish);
-
+///cout<<hungrymoves(faqua,ifish);
+cout<<hungrymovesrecur(ifish,faqua,0,0);
 return 0;
 }
