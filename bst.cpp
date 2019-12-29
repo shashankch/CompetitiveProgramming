ad65129e7c836bf8acfc817d9749476ddd499535
shashank singh <shashakchandel@gmail.com>
2019-12-29 23:09:02 +0530

    nextcommit

--- bst.cpp
+++ bst.cpp
@@ -75,9 +75,242 @@ void inorder(node *root){
 
 }
 
+void bfs(node *root){
+
+
+    queue<node *>q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty()){
+
+        node* fr=q.front();
+        if(fr==NULL){
+            cout<<endl;
+            q.pop();
+
+            if(!q.empty()){
+                q.push(NULL);
+            }
+
+        }
+        else{
+
+            cout<<fr->data<<" ";
+            q.pop();
+            if(fr->left){
+
+                q.push(fr->left);
+
+            }
+            if(fr->right){
+                q.push(fr->right);
+            }
+        }
+
+}
+return;
+
+}
+
+bool searchbst(node *root,int data){
+
+    if(root==NULL){
+
+        return false;
+    }
+
+    if(root->data==data){
+
+        return true;
+    }
+
+    if(data<=root->data){
+
+
+        return searchbst(root->left,data);
+    }
+    else{
+        return searchbst(root->right,data);
+    }
+
+
+}
+
+node * deletebst(node *root,int data){
+
+
+    if(root==NULL){
+        return NULL;
+    }
+    if(data<root->data){
+
+        root->left=deletebst(root->left,data);
+        return root;
+
+    }
+    else if(data==root->data){
+
+        ///for leaf nodes
+        if(root->left==NULL && root->right==NULL){
+
+            delete root;
+            return NULL;
+        }
+        else if(root->left!=NULL && root->right==NULL){
+
+            node *temp=root->left;
+            delete root;
+            return temp;
+
+        }
+        else if(root->right!=NULL && root->left==NULL){
+
+            node *temp=root->right;
+            delete root;
+            return temp;
+        }
+        else{
+
+
+            node *minright=root->right;
+            while(minright->left!=NULL){
+                minright=minright->left;
+            }
+
+            root->data=minright->data;
+            root->right=deletebst(root->right,minright->data);
+
+            return root;
 
 
 
+        }
+
+
+
+    }
+
+    else{
+
+        root->right=deletebst(root->right,data);
+        return root;
+    }
+
+
+
+
+
+}
+
+bool isbst(node *root,int max=INT_MAX,int min=INT_MIN){
+
+
+    if(root==NULL){
+        return true;
+    }
+
+    if(root->data>min && root->data<max && isbst(root->left,root->data,min) && isbst(root->right,max,root->data)){
+
+        return true;
+    }
+    return false;
+}
+
+class Linkedpair{
+
+public:
+    node *head;
+    node *tail;
+
+
+};
+
+Linkedpair flattentree(node *root){
+
+    Linkedpair lp;
+
+    /// no nodes
+    if(root==NULL){
+
+        lp.head=lp.tail=NULL;
+        return lp;
+    }
+    /// leaf node
+    if(root->left==NULL && root->right==NULL){
+
+        lp.head=lp.tail=root;
+
+        return lp;
+
+    }
+
+    /// node with left child..
+    if(root->left!=NULL && root->right==NULL){
+
+        Linkedpair left=flattentree(root->left);
+        left.tail->right=root;
+        lp.head=left.head;
+        lp.tail=root;
+
+        return lp;
+
+    }
+    /// node with right child
+    if(root->right!=NULL && root->left==NULL){
+
+        Linkedpair right=flattentree(root->right);
+        root->right=right.head;
+        lp.head=root;
+        lp.tail=right.tail;
+
+        return lp;
+
+    }
+
+    /// node with 2 children
+    Linkedpair l=flattentree(root->left);
+    Linkedpair r=flattentree(root->right);
+    l.tail->right=root;
+    root->right=r.head;
+    lp.head=l.head;
+    lp.tail=r.tail;
+
+    return lp;
+
+
+
+
+
+}
+
+
+node *buildbstfrompreorder(int *pre,int s,int e,int len){
+
+
+        static int k=0;
+        if(s>e || k>=len){
+            return NULL;
+        }
+
+    node *root=new node(pre[k]);
+    k++;
+    if(s==e){return root;}
+    int index;
+    for(int i=s;i<=e;i++){
+
+        if(root->data<pre[i]){
+            index=i;
+            break;
+        }
+    }
+
+    root->left=buildbstfrompreorder(pre,k,index-1,len);
+    root->right=buildbstfrompreorder(pre,index,e,len);
+
+
+    return root;
+    }
 
 
 
@@ -89,8 +322,58 @@ void inorder(node *root){
 
 int main(){
 
-node *root=buildtree();
-inorder(root);
+//node *root=buildtree();
+
+//inorder(root);
+//cout<<endl;
+//bfs(root);
+//cout<<endl;
+/*
+int data;
+cin>>data;
+if(searchbst(root,data)){
+    cout<<"Present"<<endl;
+
+}
+else{
+    cout<<"not present"<<endl;
+}
+int del;
+cin>>del;
+root=deletebst(root,del);
+bfs(root);
+cout<<endl;
+if(isbst(root)){
+    cout<<"yes";
+}
+else{
+    cout<<"no";
+}
+
+Linkedpair l=flattentree(root);
+node *temp=l.head;
+while(temp!=NULL){
+    cout<<temp->data<<"-->";
+    temp=temp->right;
+}
+*/
+int arr[]={5,3,1,7,6,8};
+int arr2[]={10,5,1,7,40,50};
+node *temp=buildbstfrompreorder(arr2,0,5,6);
+inorder(temp);
+
 
 return 0;
 }
+
+/*
+5 3 7 1 6 8 -1
+1 3 5 6 7 8
+5
+3 7
+1 6 8
+
+10
+not present
+
+*/
---
+++ catalano.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+
+
+///using dp technique
+int dp[100]={0};
+int catalan(int n){
+
+
+    if(n==0){
+        return 1;
+    }
+
+    if(dp[n]!=0){
+        return dp[n];
+    }
+
+    int ans=0;
+    for(int i=1;i<=n;i++){
+
+        ans+=catalan(i-1)*catalan(n-i);
+
+    }
+
+    dp[n]=ans;
+
+
+    return ans;
+
+
+
+}
+/// catalan number formula : 2nCn/(n+1).
+/// f(n)=summation(i=1 to n)[f(i-1)*f(n-i)].
+/// used to calculate total no. of possible bsts. from the given no. of nodes.
+///used to calculate total no.of unlabeled trees possible.
+/// for calculating total no. of binary tree: [n! * 2nCn/(n+1)]. because we can permute node data..
+/// calculating no. of expressions containing n pairs of parentheses which are correctly matched..
+
+
+
+int main(){
+
+
+int n;
+cin>>n;
+for(int i=0;i<=n;i++){
+
+    cout<<catalan(i)<<" ,";
+
+}
+
+
+
+
+
+return 0;
+}
