981366ff7dca99b0d14f12c8d7a86b58603e13ba
shashank singh <shashakchandel@gmail.com>
2020-01-05 00:21:33 +0530

    nextcommit

--- heaps.cpp
+++ heaps.cpp
@@ -126,6 +126,58 @@ void deletefromanypos(vector<int>&heap,int idx){
     downheapify(heap,0);
 }
 
+
+/// helper downheapify function : here heapsize is passed because after each swap arr.size is reduced..
+void downheapifyhelper(vector<int> &heap,int idx,int heapsize){
+
+    int left=2 *idx+1;
+    int right=2*idx+2;
+
+
+    /// to ensure it has right and left child and within the array range..
+    if(left>=heapsize and right>=heapsize){return;}
+
+    /// index of the largest value index
+    int largestidx=idx;
+
+    /// check for left and right child for highest priority.. to replace the root..
+    if(left<heapsize and heap[left]>heap[largestidx]){
+        largestidx=left;
+    }
+    if(right<heapsize and heap[right]>heap[largestidx]){
+        largestidx=right;
+    }
+    /// if root has highest priority then do nothing..
+    if(largestidx==idx){
+        return;
+    }
+
+    /// swap with the highest child ..
+    swap(heap[idx],heap[largestidx]);
+    /// call recursively to left and right tree.. where largest index
+    downheapifyhelper(heap,largestidx,heapsize);
+
+
+}
+
+/// O(nlogn) : inplace and not stable sort.. here selection takes O(1) time instead of O(n)
+
+/// here first build heap using array..by downheapify for optimized way..
+/// then reduce size of array by performing swap of first and last element and downheapification..
+
+void heapsort(vector<int>&arr){
+
+    int heapsize=arr.size();
+    buildheapfromgivenoptimal(arr);
+    for(int i=arr.size()-1;i>=0;i--){
+        swap(arr[0],arr[i]);
+        heapsize-=1;
+        downheapifyhelper(arr,0,heapsize);
+    }
+
+}
+
+
 int main(){
 
 vector<int> heap;
@@ -140,14 +192,14 @@ for(int i=0;i<n;i++){
 
 }
 display(heap);
-//buildheapfromgiven(heap);
-buildheapfromgivenoptimal(heap);
-
+///buildheapfromgiven(heap);
+///buildheapfromgivenoptimal(heap);
+heapsort(heap);
 
 ///deletepeak(heap);
 display(heap);
-deletefromanypos(heap,3);
-display(heap);
+///deletefromanypos(heap,3);
+///display(heap);
 
 
 return 0;
