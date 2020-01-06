69ede5adb35f391aeba94bdd56e2235824730a6a
shashank singh <shashakchandel@gmail.com>
2020-01-06 22:01:40 +0530

    nextcommit

--- printnodesatkdist.cpp
+++ printnodesatkdist.cpp
@@ -1,3 +1,143 @@
 
+#include<iostream>
+
+using namespace std;
+
+class node{
+
+    public:
+    int data;
+    node *left;
+    node *right;
+
+
+    node(int d){
+
+        data=d;
+        left=NULL;
+        right=NULL;
+}
+};
+
+node *buildtreeinpreorder(int *in,int *pre,int s,int e){
+
+    static int i=0;
+    if(s>e){
+        return NULL;
+    }
+
+    node *root=new node(pre[i]);
+
+    int index=-1;
+
+    for(int j=s;j<=e;j++){
+
+        if(in[j]==pre[i]){
+            index=j;
+            break;
+        }
+    }
+
+    i++;
+    root->left=buildtreeinpreorder(in,pre,s,index-1);
+    root->right=buildtreeinpreorder(in,pre,index+1,e);
+
+
+    return root;
+}
+
+
+void krootdist(node *root,int k){
+
+    if(root==NULL || k<0){
+        return ;
+    }
+    if(k==0){
+        cout<<root->data<<" ";
+        return ;
+    }
+
+    krootdist(root->left,k-1);
+    krootdist(root->right,k-1);
+
+
+}
+
+
+
+int kdistnode(node *root,int target,int k){
+
+
+    if(root==NULL){
+        return 0 ;
+    }
+    if(root->data==target){
+
+        krootdist(root,k);
+        return 0;
+
+    }
+    int ld=kdistnode(root->left,target,k);
+
+    if(ld!=-1){
+
+        if(ld+1==k){
+            cout<<root->data<<" ";
+        }
+        else{
+            krootdist(root->right,k-ld-2);
+        }
+
+        return 1+ld;
+    }
+
+     int rd=kdistnode(root->right,target,k);
+
+    if(rd!=-1){
+
+        if(rd+1==k){
+            cout<<root->data<<" ";
+        }
+        else{
+            krootdist(root->left,k-rd-2);
+        }
+
+        return 1+rd;
+    }
+
+    return -1;
+}
+
+int main(){
+
+int n;
+cin>>n;
+
+int pre[n],in[n];
+for(int i=0;i<n;i++){
+    cin>>pre[i];
+}
+
+for(int i=0;i<n;i++){
+    cin>>in[i];
+}
+
+node *temp=buildtreeinpreorder(in,pre,0,n-1);
+int q;
+cin>>q;
+while(q--){
+
+    int target,k;
+    cin>>target>>k;
+
+    kdistnode(temp,target,k);
+    cout<<endl;
+
+
+}
+
+
+return 0;
+}
 
 
--- runningmedianstreamint.cpp
+++ runningmedianstreamint.cpp
@@ -1,9 +1,103 @@
 
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
+long long int median=0;
+priority_queue<long long int> maxHeap;
+priority_queue<long long int,vector<long long int>,greater<long long int> >minHeap;
+void median2()
+{
+    if(maxHeap.size()==minHeap.size())
+    {
+        long long int sum=maxHeap.top()+minHeap.top();
+        long long int average=sum/2;
+        median=average;
+        return;
+    }
+	else if(maxHeap.size()-minHeap.size()==1)
+	{
+		median=maxHeap.top();
+		return;
+	}
+	else if(minHeap.size()-maxHeap.size()==1)
+	{
+		median=minHeap.top();
+		return;
+	}
+	else if(maxHeap.size()-minHeap.size()==2)
+	{
+		///Remove the Top of the max heap and add it into the MinHeap.
+		long long int top_element=maxHeap.top();
+		maxHeap.pop();
+		minHeap.push(top_element);
 
-int main(){
+		///Now, Size of both heapS are Same, find the average of top of the two heaps.
+		long long int sum=maxHeap.top()+minHeap.top();
+        long long int average=sum/2;
+		median=average;
+		return;
+	}
+	else if(minHeap.size()-maxHeap.size()==2)
+	{
+		///Remove the Top of the min heap and add it into the MaxHeap.
+		long long int top_element=minHeap.top();
+		minHeap.pop();
+		maxHeap.push(top_element);
 
-return 0;
-
+		///Now, Size of both heapS are Same, find the average of top of the two heaps.
+		long long int sum=maxHeap.top()+minHeap.top();
+        long long int average=sum/2;
+        median=average;
+		return;
+	}
+}
+void median1(long long int d)
+{
+	if(maxHeap.empty())
+	{
+		maxHeap.push(d);
+		median2();
+		return;
+	}
+	else if(d>median)
+	{
+		///Add to minHeap
+		minHeap.push(d);
+		median2();
+		return;
+	}
+	else if(d<median)
+	{
+		///Add to maxHeap
+		maxHeap.push(d);
+		median2();
+		return;
+	}
+}
+int main()
+{
+	long long int t;
+	cin>>t;
+	long long int i,j,d;
+	for(i=0;i<t;i++)
+	{
+        long long int n;
+		cin>>n;
+		for(j=0;j<n;j++)
+		{
+			cin>>d;
+			median1(d);
+			cout<<median<<" ";
+		}
+		while(maxHeap.size()!=0)
+		{
+			maxHeap.pop();
+		}
+		while(minHeap.size()!=0)
+		{
+			minHeap.pop();
+		}
+        median=0;
+		cout<<"\n";
+	}
 }
