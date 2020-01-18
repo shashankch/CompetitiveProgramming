33500ea1f1421b6738a9bb31d8ca4293a5542ca0
shashank singh <shashakchandel@gmail.com>
2020-01-18 23:46:34 +0530

    nextcommit

---
+++ chopstickschef.cpp
@@ -0,0 +1,35 @@
+///Problem Code: TACHSTCK CODECHEF
+
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+
+int main(){
+
+ll n,k;
+cin>>n>>k;
+vector<ll>v;
+for(ll i=0;i<n;i++){
+        ll x;
+    cin>>x;
+    v.push_back(x);
+}
+sort(v.begin(),v.end());
+ll count=0;
+for(ll i=0;i<n-1;i++){
+
+    if(v[i+1]-v[i]<=k && v[i+1]-v[i]!=0){
+        count++;
+        i++;
+
+    }
+    else{
+        continue;
+    }
+
+}
+cout<<count;
+
+return 0;
+}
--- deepakandjourney.cpp
+++ deepakandjourney.cpp
@@ -1,1 +1,71 @@
 
+#include<iostream>
+#include<vector>
+#define ll long long int
+using namespace std;
+
+
+
+int main(){
+
+ll t;
+cin>>t;
+while(t--){
+ll n;
+cin>>n;
+vector<ll>c(n);
+vector<ll>l(n);
+vector<ll>m(n);
+for(ll i=0;i<n;i++){
+    ll cost;
+    cin>>cost;
+    c[i]=cost;
+    if(i==0){
+        m[i]=c[i];
+    }
+    else{
+        m[i]=min(m[i-1],c[i]);
+    }
+}
+for(int i=0;i<n;i++){
+    cin>>l[i];
+}
+int finalcost=0;
+/// int petrolavl=0;
+for(int i=0;i<n;i++){
+    finalcost+=l[i]*m[i];
+}
+cout<<finalcost<<endl;
+
+
+}
+return 0;
+
+}
+/*
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t,n,min,sum;
+    cin>>t;
+    while(t--)
+    {
+    cin>>n;
+    int*c=new int[n];
+    int*l=new int[n];
+    for(int i=0;i<n;i++) cin>>c[i];
+	for(int i=0;i<n;i++) cin>>l[i];
+	min=c[0];sum=0;
+	for(int i=0;i<n;i++)
+	{
+     if(c[i]<min)
+	 min=c[i];
+     sum=sum+min*l[i];
+	}
+	cout<<sum<<endl;
+    }
+	return 0;
+}
+
+
+*/
